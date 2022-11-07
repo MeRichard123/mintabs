dd3bf2bca650774863f9bc01249fc10eb742074d
Richard <rcoric03@gmail.com>
2022-11-07 00:59:09 +0000

    Add Personal Links

--- README.md
+++ README.md
@@ -22,3 +22,24 @@ npm run lint
 
 ### Customize configuration
 See [Configuration Reference](https://cli.vuejs.org/config/).
+
+### Deployment
+```shell
+# build the code
+npm run build
+
+# add the build
+git add dist
+
+# commit the build 
+git commit -m "Adding new Build"
+
+# push a subtree
+git subtree push --prefix dist origin gh-pages
+
+# then push the rest
+git aa
+git cm "Modify []"
+git push -u origin main
+
+```
--- src/views/PersonalView.vue
+++ src/views/PersonalView.vue
@@ -1,11 +1,37 @@
 <template>
-<h1>Personal</h1>
+    <section class="flexBasic flex-wrap">
+        <a v-for="link in links" :key="link.name" :href="link.link">
+            <div class="p-8 text-2xl m-5 card text-center themeText flexBasic">
+                <h3>{{link.name}}</h3>
+            </div>
+        </a>
+    </section>
 </template>
 
 <script>
 
 export default {
   name: 'PersonalView',
+  data() {
+    return {
+      links: [
+        { name: 'Netflix', link: 'https://www.netflix.com/browse' },
+        { name: 'Youtube', link: 'https://www.youtube.com/' },
+        { name: 'Discord', link: 'https://discord.com/' },
+      ],
+    };
+  },
 
 };
 </script>
+<style>
+    .card{
+        width: 220px;
+        height: 220px;
+        box-shadow: rgba(149, 157, 165, 0.2) 0px 8px 24px;
+    }
+    .card:hover{
+        box-shadow: rgba(100, 100, 111, 0.2) 0px 7px 29px 0px;
+    }
+
+</style>
